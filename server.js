7df50c02d2bc817428f07df4c811f43d271b5357
Kris <kriskuiper74@gmail.com>
2019-08-08 20:37:59 +0200

    feat(project): config scripts

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require('express')
 
+const port = 5454;
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Home')
+})
+
+app.listen(port, () => {
+  console.log(`Development server available on http://localhost:${port}`)
+})
