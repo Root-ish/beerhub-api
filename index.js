ce48697fc0682a80a38ea00d0b6e24e41e79d11c
Rick <rickmichiel@icloud.com>
2019-08-22 21:18:55 +0200

    resolved some issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const mongoose = require("mongoose")
-require("dotenv").config()
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const port = 5454
 const app = express()
@@ -9,8 +9,18 @@ const authRoute = require('./routes/auth')
 
 // Process environment uri and connect to database
 const uri = process.env.MONGODB_URI
-mongoose.set("useNewUrlParser", true)
-mongoose.connect(uri)
+
+
+function handleConnectionError (){
+  console.log('500 server') // Add proper error handeling.
+}
+
+function logError (){
+  console.log('500 server') // Add proper error handeling.
+}
+
+mongoose.connect(uri, {'useNewUrlParser': true}).catch(handleConnectionError)
+mongoose.connection.on('error', logError)
 
 // Routes
 app.use('/api/user', authRoute)
--- models/User.js
+++ models/User.js
@@ -1,11 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const {ObjectId, Schema} = mongoose
 
 const userSchema = new Schema({
     _id: ObjectId,
-    email: String,
-    password: String,
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
     beers: Array
 })
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model('User', userSchema)
