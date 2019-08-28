1146260c95060a2caa483bf599c283e7f670f0a6
Gebruiker <Gebruiker@Gebruikers-iMac.local>
2019-08-28 20:59:25 +0200

    test(auth): set up tests for auth route

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,9 +1,36 @@
-const { assert } = require('chai')
+require('dotenv').config()
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
+const { API_BASEURL } = process.env
+
+chai.use(chaiHttp)
+
+const request = chai.request(API_BASEURL)
+const { expect } = chai
 
 describe('Auth test', () => {
   it('Can register a user via API', () => {
+    // request
+    //   .post('/user/register')
+    //   .send({
+    //     username: 'testMcTest',
+    //     email: 'testmctest@test.com',
+    //     password: 'doesitstillwork?'
+    //   })
+    //   .then(res => {
+    //     expect(res).to.have.status(200)
+    //   })
+    //   .catch(err => {
+    //     throw err
+    //   })
+  })
+
+  it('Can login via API', () => {
 
   })
 
-  it('Can login via API')
+  it('Throws error when email exists', () => {
+
+  })
 })
