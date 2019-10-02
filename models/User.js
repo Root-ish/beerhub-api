d326376d970800c4e8e34881ee8caddbb6dfa003
Rick <rickmichiel@icloud.com>
2019-10-02 20:45:23 +0200

    chore: update user schema

--- models/User.js
+++ models/User.js
@@ -1,20 +1,33 @@
 const mongoose = require('mongoose')
+
 const { ObjectId, Schema } = mongoose
 
 const userSchema = new Schema({
-    _id: ObjectId,
-    email: {
-      type: String,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    beers:  {
-      type: Array,
-      default: []
-    }
+  _id: ObjectId,
+  untappdId: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  LastName: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  beers: {
+    type: Array,
+    default: [],
+  },
 })
 
 module.exports = mongoose.model('User', userSchema)
